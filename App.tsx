23257cc7bbf794286cbc1d236f45afc93f79c834
Alex <alex.sionis846@gmail.com>
2023-06-17 02:45:15 +0300

    add: redux

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent!! </Text>
-      </View>
+      <Provider store={store}>
+        <View>
+          <Text> textInComponent!! </Text>
+        </View>
+      </Provider>
     );
   }
 }
